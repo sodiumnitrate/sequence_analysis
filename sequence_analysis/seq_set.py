9e12843aee8d79f408966e98cf2787e77c6b27cc
sodiumnitrate <irem.altan@gmail.com>
2023-01-10 19:36:31 -0500

    added basic methods

--- sequence_analysis/seq_set.py
+++ sequence_analysis/seq_set.py
@@ -2,12 +2,38 @@
 This file holds the sequence set (seq_set) class and attributed methods.
 Includes I/O of .fasta files.
 """
+from Bio import SeqIO
 
 
 class seq_set:
-    def __init__(self,list_of_sequences=None):
+    def __init__(self,list_of_sequences=[]):
         self.records = list_of_sequences
+        self.names = None
 
     def write_fasta(self,file_name):
-        for seq in self.records:
-            pass
+        # function to write all sequences within the list to a fasta file
+        f = open(file_name,'w')
+        for i,seq in self.records:
+            s = seq.seq
+            if self.names is not None:
+                record_name = self.names[i]
+            else:
+                record_name = f"record|{i}"
+            name = "> " + record_name 
+            f.write(name)
+            f.write('\n')
+            for ct, char in enumerate(s):
+                if ct % 79 == 0 and ct != 0:
+                    f.write('\n')
+                f.write(char)
+            f.write('\n')
+        f.close()
+
+
+    def read_fasta(self,file_name):
+        if len(self.records) > 0:
+            print("Warning: overwriting existing data")
+        for record in SeqIO.parse(file_name,"fasta"):
+            seq = sequence(str(record.seq), record.name)
+            self.records.append(seq)
+
--- sequence_analysis/sequence.py
+++ sequence_analysis/sequence.py
@@ -3,5 +3,6 @@ This file holds the sequence class and related methods.
 """
 
 class sequence:
-    def __init__(self,seq):
+    def __init__(self,seq,name=None):
         self.seq = seq
+        self.name = name
