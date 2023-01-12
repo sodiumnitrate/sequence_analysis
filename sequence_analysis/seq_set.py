e9e333afe17c5140197ccc7251b5404199e595e0
sodiumnitrate <irem.altan@gmail.com>
2023-01-12 10:06:45 -0500

    added sequence type detection

--- sequence_analysis/seq_set.py
+++ sequence_analysis/seq_set.py
@@ -5,13 +5,26 @@ Includes I/O of .fasta files.
 from Bio import SeqIO
 from sequence_analysis.sequence import sequence
 import pdb
+from sequence_analysis.utils import aa_alphabet
+from sequence_analysis.utils import dna_alphabet
+from sequence_analysis.utils import rna_alphabet
+from sequence_analysis.utils import diff_letters
 
 class seq_set:
-    def __init__(self,list_of_sequences=[],file_name=None):
+    def __init__(self, list_of_sequences=None, file_name=None, type=None):
+        if list_of_sequences is None:
+            list_of_sequences = []
         self.records = list_of_sequences
+        self.type = type
+
+        if len(list_of_sequences) == 0 and file_name is None:
+            print("WARNING: sequence set initialized without sequences or file name to read from. Please use read_fasta() to read in sequences or use add_sequence() to add sequences.")
 
         if file_name is not None:
             self.read_fasta(file_name)
+
+        if type is None and len(self.records) > 0:
+            self.set_type()
 
     def write_fasta(self,file_name):
         # function to write all sequences within the list to a fasta file
@@ -28,6 +41,33 @@ class seq_set:
             f.write('\n')
         f.close()
 
+    def get_letters(self):
+        all_letters = set()
+        for seq in self.records:
+            s = seq.seq
+            all_letters = all_letters.union(set([*s]))
+        return all_letters
+
+    def set_type(self,type=None):
+        # TODO: do further testing of type detection
+        all_letters = self.get_letters()
+        
+        if type is not None:
+            self.type = type
+            # check that the assigned type is consistent
+            return
+
+        if len(diff_letters.intersection(all_letters)):
+            self.type = 'protein'
+        elif set(dna_alphabet).issubset(all_letters) or set(rna_alphabet).issubset(all_letters):
+            if 'U' in all_letters:
+                self.type = 'rna'
+            else:
+                # this could fail if we have RNA sequences that happens to have no Us, but that's unlikely
+                self.type = 'dna'
+        else:
+            print("I can't assign a type, please specify manually.")
+
 
     def read_fasta(self,file_name):
         if len(self.records) > 0:
@@ -36,6 +76,22 @@ class seq_set:
             seq = sequence(str(record.seq), record.name)
             self.records.append(seq)
 
-    def add_sequence(self,sequence_obj):
-        self.records.append(sequence_obj)
+        self.set_type()
+
+    def add_sequence(self,sequences):
+        if isinstance(sequences, list):
+            for seq in sequences:
+                assert(isinstance(seq, sequence))
+                self.records.append(seq)
+            old_type = self.type
+            self.set_type()
+            assert(old_type == self.type)
+        elif isinstance(sequences, sequence):
+            self.records.append(sequences)
+            old_type = self.type
+            assert(old_type == self.type)
+            self.set_type()
+        else:
+            print("ERROR: incorrect format for adding sequence. Please provide a list of sequences or a sequence object. Sequence was not added.")
+
 
--- sequence_analysis/utils.py
+++ sequence_analysis/utils.py
@@ -2,5 +2,10 @@
 This file holds some basic tools and definitions, mostly for sequence I/O.
 """
 
-aas = ['A','R','N','D','C','E','Q','G','H','I','L','K','M','F','P','S','T','W','Y','V']
+aa_alphabet = ['A','R','N','D','C','E','Q','G','H','I','L','K','M','F','P','S','T','W','Y','V']
 
+dna_alphabet = ['G','C','T','A']
+rna_alphabet = ['G','C','U','A']
+
+# letters in aa alphabet but not in dna or rna
+diff_letters = set(aa_alphabet) - (set(dna_alphabet).union(set(rna_alphabet)))
