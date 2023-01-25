c2c6f7c24e7c12856ef95a2fe31e25b4e675d483
sodiumnitrate <irem.altan@gmail.com>
2023-01-25 12:29:54 -0500

    implemented custom scoring in global alignments. TODO: local alignments

--- sequence_analysis/pairwise_alignment.py
+++ sequence_analysis/pairwise_alignment.py
@@ -4,7 +4,7 @@ import Bio.pairwise2 as biopython_pairwise2
 from sequence_analysis.utils import blosum_50
 
 class pairwise_alignment:
-    def __init__(self, sequence1, sequence2, algorithm="needleman-wunsch"):
+    def __init__(self, sequence1, sequence2, match=1, unmatch=0, gap=-8, gap_open=-0.5, use_blosum_50=True, algorithm="needleman-wunsch"):
         # TODO: add support for different scores
         if isinstance(sequence1, str):
             sequence1 = sequence(sequence1)
@@ -12,6 +12,16 @@ class pairwise_alignment:
             sequence2 = sequence(sequence2)
         self.sequence1 = sequence1
         self.sequence2 = sequence2
+
+        # what parameters to use. 
+        # if use_blosum_50 is True, the match and unmatch params are ignored
+        self.use_blosum_50 = use_blosum_50
+        self.match = match
+        self.unmatch = unmatch
+        # gap is -8 by default, which is chosen to play nicely with blosum50.
+        # if using custom match and unmatch scores, make sure the chosen gap penalty is reasonable
+        self.gap = gap
+        self.gap_open = gap_open
 
         self.algorithm = algorithm
 
@@ -21,30 +31,42 @@ class pairwise_alignment:
         self.F = None
         self.pointers = None
 
-    def align(self,d=8,verbose=False,score_only=False):
+    def align(self,verbose=False,score_only=False):
+        # TODO: d no longer needs to be passed as a function argument. FIX.
+        d = self.gap
         if self.algorithm == "needleman-wunsch":
-            self.needleman_wunsch(d=d,verbose=verbose)
+            self.needleman_wunsch(verbose=verbose)
         elif self.algorithm == "smith-waterman":
-            self.smith_waterman(d=d, verbose=verbose)
+            self.smith_waterman(verbose=verbose)
         elif self.algorithm == "biopython-global":
-            # TODO: figure out how to deal with the score-only option
-            self.biopython_global(d=8,score_only=score_only)
+            # TODO: is this the best way of dealing with the score-only option?
+            self.biopython_global(score_only=score_only)
 
-    def biopython_global(self,d=8,score_only=True):
+    def biopython_global(self,score_only=True):
         seq1 = self.sequence1.seq
         seq2 = self.sequence2.seq
 
-        if score_only:
-            alignment = biopython_pairwise2.align.globalds(seq1, seq2, blosum_50, -1*d, -1*d, score_only=True)
-            self.score = alignment
+        if self.use_blosum_50:
+            if score_only:
+                alignment = biopython_pairwise2.align.globalds(seq1, seq2, blosum_50, self.gap, self.gap, score_only=True)
+                self.score = alignment
+            else:
+                alignment = biopython_pairwise2.align.globalds(seq1, seq2, blosum_50, self.gap, self.gap)
+                self.score = alignment[0].score
+                self.sequence1_aligned = sequence(alignment[0].seqB)
+                self.sequence1_aligned = sequence(alignment[0].seqA)
         else:
-            alignment = biopython_pairwise2.align.globalds(seq1, seq2, blosum_50, -1*d, -1*d)
-            self.score = alignment[0].score
-            self.sequence1_aligned = sequence(alignment[0].seqB)
-            self.sequence1_aligned = sequence(alignment[0].seqA)
+            if score_only:
+                alignment = biopython_pairwise2.align.globalms(seq1, seq2, self.match, self.unmatch, self.gap_open, self.gap, score_only=True)
+                self.score = alignment
+            else:
+                alignment = biopython_pairwise2.align.globalms(seq1, seq2, self.match, self.unmatch, self.gap_open, self.gap)
+                self.score = alignment[0].score
+                self.sequence1_aligned = sequence(alignment[0].seqB)
+                self.sequence2_aligned = sequence(alignment[0].seqA)
 
 
-    def needleman_wunsch(self,d=8,verbose=False):
+    def needleman_wunsch(self,verbose=False):
         # get lengths of sequences
         n = len(self.sequence1.seq)
         m = len(self.sequence2.seq)
@@ -55,43 +77,76 @@ class pairwise_alignment:
 
         # initialize the boundaries
         for i in range(m+1):
-            F[i][0] = -i * d
+            F[i][0] = i * self.gap
             pointers[i][0] = 'u'
 
         for i in range(n+1):
-            F[0][i] = -i * d
+            F[0][i] = i * self.gap
             pointers[0][i] = 'l'
 
         pointers[0][0] = ""
 
         # TODO: rewrite this in C++ because nested for loops
         # i indexes into y, j indexes into x
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                possible_values = []
+        if self.use_blosum_50:
+            # TODO: REFACTOR
+            for i in range(1,m+1):
+                for j in range(1,n+1):
+                    possible_values = []
 
-                # x_j and y_i are aligned
-                aa1 = self.sequence1.seq[j-1]
-                aa2 = self.sequence2.seq[i-1]
-                possible_values.append(F[i-1][j-1] + query_blosum50(aa1,aa2))
+                    # x_j and y_i are aligned
+                    aa1 = self.sequence1.seq[j-1]
+                    aa2 = self.sequence2.seq[i-1]
+                    possible_values.append(F[i-1][j-1] + query_blosum50(aa1,aa2))
 
-                # x_i is aligned to a gap
-                possible_values.append(F[i-1][j] - d)
+                    # x_i is aligned to a gap
+                    possible_values.append(F[i-1][j] + self.gap)
 
-                # y_j is aligned to a gap
-                possible_values.append(F[i][j-1] - d)
+                    # y_j is aligned to a gap
+                    possible_values.append(F[i][j-1] + self.gap)
 
-                # choose option that maximizes the cumulative score
-                max_val = max(possible_values)
-                max_ind = possible_values.index(max_val)
+                    # choose option that maximizes the cumulative score
+                    max_val = max(possible_values)
+                    max_ind = possible_values.index(max_val)
 
-                F[i][j] = max_val
-                if max_ind == 0:
-                    pointers[i][j] = "ul"
-                elif max_ind == 1:
-                    pointers[i][j] = "u"
-                else:
-                    pointers[i][j] = "l"
+                    F[i][j] = max_val
+                    if max_ind == 0:
+                        pointers[i][j] = "ul"
+                    elif max_ind == 1:
+                        pointers[i][j] = "u"
+                    else:
+                        pointers[i][j] = "l"
+        else:
+            for i in range(1,m+1):
+                for j in range(1,n+1):
+                    possible_values = []
+
+                    # x_j and y_i are aligned
+                    aa1 = self.sequence1.seq[j-1]
+                    aa2 = self.sequence2.seq[i-1]
+                    if aa1 == aa2:
+                        sc = self.match
+                    else:
+                        sc = self.unmatch
+                    possible_values.append(F[i-1][j-1] + sc)
+
+                    # x_i is aligned to a gap
+                    possible_values.append(F[i-1][j] + self.gap)
+
+                    # y_j is aligned to a gap
+                    possible_values.append(F[i][j-1] + self.gap)
+
+                    # choose option that maximizes the cumulative score
+                    max_val = max(possible_values)
+                    max_ind = possible_values.index(max_val)
+
+                    F[i][j] = max_val
+                    if max_ind == 0:
+                        pointers[i][j] = "ul"
+                    elif max_ind == 1:
+                        pointers[i][j] = "u"
+                    else:
+                        pointers[i][j] = "l"
 
         self.score = F[-1][-1]
         self.F = F
@@ -152,7 +207,7 @@ class pairwise_alignment:
         for line in matrix:
             print(line)
 
-    def smith_waterman(self,d=8,verbose=False):
+    def smith_waterman(self,verbose=False):
         n = len(self.sequence1.seq)
         m = len(self.sequence2.seq)
 
@@ -174,10 +229,10 @@ class pairwise_alignment:
                 possible_values.append(F[i-1][j-1] + query_blosum50(aa1,aa2))
 
                 # x_i is aligned to gap
-                possible_values.append(F[i-1][j] - d)
+                possible_values.append(F[i-1][j] + self.gap)
 
                 # y_j is aligned to gap
-                possible_values.append(F[i][j-1] - d)
+                possible_values.append(F[i][j-1] + self.gap)
 
                 # choose option that maximizes the cumulative score
                 max_val = max(possible_values)
--- tests/test_pairwise_alignment.py
+++ tests/test_pairwise_alignment.py
@@ -1,4 +1,4 @@
-from hashlib import new
+from matplotlib import use
 from sequence_analysis.pairwise_alignment import pairwise_alignment
 
 class TestPairwiseAlignment:
@@ -72,7 +72,20 @@ class TestPairwiseAlignment:
         alignment_bp.align()
 
         assert(alignment_nw.score == alignment_bp.score)
+        # NOTE: in general, it's unreasonable to expect the following to match because of the ambiguity in situations where two scores are equal while forming F_ij. It does work out for this particular example, though.
         assert(alignment_nw.sequence1_aligned.seq == alignment_bp.sequence1_aligned.seq)
+
+    def test_biopython_vs_needleman_wunsch_custom_params(self):
+        seq1 = "HEAGAWGHEE"
+        seq2 = "PAWHEAE"
+        alignment_nw = pairwise_alignment(seq1,seq2, match=2, unmatch=-1,gap=-0.1,gap_open=-0.1,algorithm="needleman-wunsch", use_blosum_50=False)
+        alignment_nw.align()
+        alignment_bp = pairwise_alignment(seq1,seq2,match=2, unmatch=-1,gap=-0.1,gap_open=-0.1,algorithm="biopython-global", use_blosum_50=False)
+        alignment_bp.align()
+
+        assert(alignment_nw.score == alignment_bp.score)
+        # TODO: extend function to access all possibilities within the list of alignments biopython returns?
+        #assert(alignment_nw.sequence1_aligned.seq == alignment_bp.sequence1_aligned.seq)
 
 
     # TODO: check empty string or string of numbers, etc.
